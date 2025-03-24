ce3927120c499d101b413fabf8a434c2c0bec403
NLV <europagronau@gmail.com>
2025-03-24 16:26:37 +0300

    check

--- script/index.js
+++ script/index.js
@@ -41,3 +41,16 @@ if (menu.style.display === 'block') {
 }
 }
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+      if (targetElement) {
+          window.scrollTo({
+              top: targetElement.offsetTop,
+              behavior: "smooth"
+          });
+      }
+  });
+});
